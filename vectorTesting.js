1a45972329e146e0a045e6db884a5b04c807cdbf
Alex Thompson <alexthomps11@gmail.com>
2019-07-10 19:57:05 -0400

    full vector playground implemented

--- vectorTesting.js
+++ vectorTesting.js
@@ -1,7 +1,70 @@
 let a = new Vector(100, 100);
 console.log(a);
-let dotty = document.createElement("div");
-dotty.classList.add("dots");
-dotty.style.left = a.posX + "px";
-dotty.style.top = a.posY + "px";
-dotty.innerHTML = document.body.append(dotty);
+let dottya = document.createElement("div");
+dottya.classList.add("dots");
+dottya.style.left = a.posX + "px";
+dottya.style.top = a.posY + "px";
+dottya.innerHTML = `x:${a.posX} y:${a.posY}`;
+document.body.appendChild(dottya);
+
+let b = new Vector(300, 200);
+console.log(b);
+let dottyb = document.createElement("div");
+dottyb.classList.add("dots");
+dottyb.style.left = b.posX + "px";
+dottyb.style.top = b.posY + "px";
+dottyb.innerHTML = `x:${b.posX} y:${b.posY}`;
+document.body.appendChild(dottyb);
+
+let dAtoB = a.calcDistance(b);
+let dBtoA = b.calcDistance(a);
+
+let magA = a.calcMagnitude();
+let magB = b.calcMagnitude();
+
+let unA = a.getUnitVector();
+let unB = b.getUnitVector();
+
+console.log(`dAtoB: ${dAtoB}`);
+console.log(`dBtoA: ${dBtoA}`);
+console.log(`magA: ${magA}`);
+console.log(`magB: ${magB}`);
+
+console.log(`unA`);
+console.log(unA);
+console.log(unA.calcMagnitude());
+
+console.log(`unB`);
+console.log(unB);
+console.log(unB.calcMagnitude());
+
+let unAtoB = a.getUnitVectorTo(b);
+console.log(`unAtoB`);
+console.log(unAtoB);
+console.log(unAtoB.calcMagnitude());
+
+let unBtoA = b.getUnitVectorTo(a);
+console.log(`unBtoA`);
+console.log(unBtoA);
+console.log(unBtoA.calcMagnitude());
+
+// setInterval(function() {
+//   a.addVector(unAtoB);
+//   dottya.style.left = a.posX + "px";
+//   dottya.style.top = a.posY + "px";
+// }, 50);
+let clickVector;
+let unAtoClickVector;
+document.addEventListener("click", function(evt) {
+  console.log(evt);
+  clickVector = new Vector(evt.clientX, evt.clientY);
+  unAtoClickVector = a.getUnitVectorTo(clickVector);
+  console.log("untoaclickvector");
+  console.log(unAtoClickVector);
+});
+
+setInterval(function() {
+  a.addVector(unAtoClickVector);
+  dottya.style.left = a.posX + "px";
+  dottya.style.top = a.posY + "px";
+}, 50);
--- vectors.js
+++ vectors.js
@@ -1,6 +1,7 @@
 const Vector = function(x, y) {
   this.posX = x;
   this.posY = y;
+  this.mag = this.calcMagnitude;
 
   this.calcDistance = function(vector) {
     return Math.sqrt(
@@ -21,6 +22,15 @@ const Vector = function(x, y) {
   };
 
   this.getUnitVectorTo = function(vector) {
+    // console.log("this");
+    // console.log(this);
+    // console.log("vector");
+    // console.log(vector);
+    // console.log(`this.posX:${this.posX}`);
+    // console.log(`this.posY:${this.posY}`);
+    // console.log(`vector.posX:${vector.posX}`);
+    // console.log(`vector.posY:${vector.posY}`);
+
     return new Vector(
       (vector.posX - this.posX) / this.calcDistance(vector),
       (vector.posY - this.posY) / this.calcDistance(vector)
